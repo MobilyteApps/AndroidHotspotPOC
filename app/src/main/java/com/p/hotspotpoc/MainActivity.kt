598ff968cb3e8d5affc88220ca6ac0306820ac74
Vishnu Sharma <vishnudbimcs@gmail.com>
2020-04-18 08:28:26 +0530

    crash issue fixed on Samsung S9+

--- app/src/main/java/com/p/hotspotpoc/MainActivity.kt
+++ app/src/main/java/com/p/hotspotpoc/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.p.hotspotpoc
 
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.location.LocationManager
 import android.net.wifi.WifiManager.LocalOnlyHotspotReservation
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,29 +23,71 @@ class MainActivity : AppCompatActivity() {
         Log.d("state",a.toString())
 
         checkStatus()
+        // Crashlytics.getInstance().crash()
 
         ActivityCompat.requestPermissions(this,
             arrayOf(
                 android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_NETWORK_STATE,
+                android.Manifest.permission.CHANGE_NETWORK_STATE,
+                android.Manifest.permission.ACCESS_WIFI_STATE,
+                android.Manifest.permission.CHANGE_WIFI_STATE
             ), 1
         )
 
         btnOn.setOnClickListener {
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
-               mReservation= ApManager.configApStateOreo(this)
-            }else {
-                ApManager.configApState(this)
-            }
-            txtStatus.text="Hotspot is On"}
+            checkLocation()
+
+        }
         btnOff.setOnClickListener {
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
                 ApManager.close()
             }else {
-                ApManager.configApState(this)
+                ApManager.configApState(applicationContext)
             }
 
             txtStatus.text="Hotspot is Off" }
+    }
+
+    private fun checkLocation() {
+        val lm =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        var gps_enabled = false
+        var network_enabled = false
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        } catch (ex: Exception) {
+        }
+
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        } catch (ex: Exception) {
+        }
+
+        if (!gps_enabled && !network_enabled) { // notify user
+            AlertDialog.Builder(this)
+                .setMessage("Gps not enabled")
+                .setPositiveButton(
+                    "open settings",
+                    DialogInterface.OnClickListener { paramDialogInterface, paramInt ->
+                        startActivity(
+                            Intent(
+                                Settings.ACTION_LOCATION_SOURCE_SETTINGS
+                            )
+                        )
+                    })
+                .setNegativeButton("cancel", null)
+                .show()
+        } else {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                mReservation = ApManager.configApStateOreo(this)
+            } else {
+                ApManager.configApState(applicationContext)
+            }
+            txtStatus.text = "Hotspot is On"
+        }
     }
 
     private fun checkStatus() {
